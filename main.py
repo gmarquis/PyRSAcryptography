ddb6d7f4a61865a41c70d7d9b58d9cf30d0bc9be
Glen Marquis <glen@Glens-MacBook-Air.local>
2023-08-12 13:26:31 +0100

    encode public, decode using private key

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import rsa
 
-## Generate private and public keys
-#public_key, private_key = rsa.newkeys(1024) ## MIICYAIBAAKBgQC
-#with open("public.pem", "wb") as f:
-# f.write(public_key.save_pkcs1("PEM"))
-#with open("private.pem", "wb") as f:
-# f.write(private_key.save_pkcs1("PEM"))
+    ## Generate private and public keys ##
+public_key, private_key = rsa.newkeys(1024)
+with open("public.pem", "wb") as f:
+ f.write(public_key.save_pkcs1("PEM"))
+with open("private.pem", "wb") as f:
+ f.write(private_key.save_pkcs1("PEM"))
 
+    ## Use Public key to encode ##
 with open("public.pem", "rb") as f:
-    public_key = rsa.PublicKey.load_pkcs1(f.read())
-##with open("private.pem", "rb") as f:
-##    private_key = rsa.PrivateKey.load_pkcs1(f.read())
-
-message = "Hello my new password is neural_ninetynine"
-encrypted_message = rsa.encrypt(message.encode(), public_key)
-with open("encrypted_message", "wb") as f:
+ public_key = rsa.PublicKey.load_pkcs1(f.read())
+with open("private.pem", "rb") as f:
+ private_key = rsa.PrivateKey.load_pkcs1(f.read())
+ message = "Hello my password is neural_ninety-four"
+ encrypted_message = rsa.encrypt(message.encode(), public_key)
+    ## Write encode message to disk ##
+ with open("encrypted_message", "wb") as f:
     f.write(encrypted_message)
+
+    ## Use Private key to decode ##
+with open("private.pem", "rb") as f:
+    private_key = rsa.PrivateKey.load_pkcs1(f.read())
+encrypted_message = open("encrypted_message", "rb").read()
+decoded_message = rsa.decrypt(encrypted_message, private_key)
+print(decoded_message.decode())
+
